6e4f54485c9a91117b33c83a36c3fa845f07a564
Benjamin Wilson <benjamin.blake.wilson13@gmail.com>
2023-03-14 21:06:43 -0500

    some crappy security

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require('http');
-const fs = require('fs'); 
+const fs = require('fs');
 const { url } = require('inspector');
 
-const signed = []; 
+const signed = [];
 
 const server = http.createServer((request, response) => {
     console.log(request.method, request.url)
@@ -13,7 +13,7 @@ const server = http.createServer((request, response) => {
         console.log(request.method, request.url, 'asdf');
         requestBody += data;
     });
-    console.log(requestBody); 
+    console.log(requestBody);
     request.on('end', () => {
         if (requestBody) {
             request.body = requestBody
@@ -29,80 +29,98 @@ const server = http.createServer((request, response) => {
 
         if (request.method === 'GET') {
             if (request.url === '/' || request.url === '/index.html') {
-                const htmlReturn = fs.readFileSync('./index.html', 'utf-8'); 
-                response.statusCode = 200; 
-                response.setHeader('Content-Type', 'text/html'); 
-                response.write(htmlReturn); 
-                return response.end(); 
+                const htmlReturn = fs.readFileSync('./index.html', 'utf-8');
+                response.statusCode = 200;
+                response.setHeader('Content-Type', 'text/html');
+                response.write(htmlReturn);
+                return response.end();
             }
 
             if (request.url === '/styles.css') {
-                const cssReturn = fs.readFileSync('./styles.css', 'utf-8'); 
-                response.statusCode = 200; 
-                response.setHeader('Content-Type', 'text/css'); 
-                response.write(cssReturn); 
-                return response.end(); 
+                const cssReturn = fs.readFileSync('./styles.css', 'utf-8');
+                response.statusCode = 200;
+                response.setHeader('Content-Type', 'text/css');
+                response.write(cssReturn);
+                return response.end();
             }
 
             if (request.url === '/page2.html') {
-                let htmlReturn = fs.readFileSync('./page2.html', 'utf-8'); 
-                response.statusCode = 200; 
-                response.setHeader('Content-Type', 'text/html'); 
-                
-                let guestList = ''; 
-                console.log(signed); 
+                let htmlReturn = fs.readFileSync('./page2.html', 'utf-8');
+                response.statusCode = 200;
+                response.setHeader('Content-Type', 'text/html');
+
+                let guestList = '';
+                console.log(signed);
+                let firstname = ''; 
+                let lastname = ''; 
+                let text = ''; 
                 for (let element of signed) {
-                    guestList += `<li>First Name: ${element.first_name}     Last Name: ${element.last_name} <ul> <li>Some Text: ${element.description}</li></ul></li>`
+                    firstname = element.first_name; 
+                    lastname = element.last_name; 
+                    text = element.description; 
+                    while (true) {
+                        firstname = firstname.replace('<', '');
+                        lastname = lastname.replace('<', '');
+                        text = text.replace('<', '');
+                        firstname = firstname.replace('>', '');
+                        lastname = lastname.replace('>', '');
+                        text = text.replace('>', '');                      
+
+                        if (firstname.indexOf('<') === -1 && lastname.indexOf('<') === -1 && text.indexOf('<') === -1) {
+                            break; 
+                        }
+                    }
+                    guestList += `<li>First Name: ${firstname}     Last Name: ${lastname} <ul> <li>Some Text: ${text}</li></ul></li>`
                 }
-                htmlReturn = htmlReturn.replace('#{signee}', guestList); 
-                response.write(htmlReturn); 
-                return response.end(); 
+                htmlReturn = htmlReturn.replace('#{signee}', guestList);
+                response.write(htmlReturn);
+                return response.end();
             }
 
             if (request.url === '/page3.html') {
-                const htmlReturn = fs.readFileSync('./page3.html', 'utf-8'); 
-                response.statusCode = 200; 
-                response.setHeader('Content-Type', 'text/html'); 
-                response.write(htmlReturn); 
-                return response.end(); 
+                const htmlReturn = fs.readFileSync('./page3.html', 'utf-8');
+                response.statusCode = 200;
+                response.setHeader('Content-Type', 'text/html');
+                response.write(htmlReturn);
+                return response.end();
             }
 
             if (request.url === '/page4.html') {
-                const htmlReturn = fs.readFileSync('./page4.html', 'utf-8'); 
-                response.statusCode = 200; 
-                response.setHeader('Content-Type', 'text/html'); 
-                response.write(htmlReturn); 
-                return response.end(); 
+                const htmlReturn = fs.readFileSync('./page4.html', 'utf-8');
+                response.statusCode = 200;
+                response.setHeader('Content-Type', 'text/html');
+                response.write(htmlReturn);
+                return response.end();
             }
 
-            const urlSplit = request.url.split('/'); 
-            urlSplit.splice(0, 1); 
-            console.log(urlSplit); 
-            
+            const urlSplit = request.url.split('/');
+            urlSplit.splice(0, 1);
+            console.log(urlSplit);
+
             if (urlSplit[0] === 'images') {
-                response.statusCode = 200; 
+                response.statusCode = 200;
                 response.setHeader('Content-Type', 'image/jpg')
                 interpoBoi = urlSplit.join('/')
-                console.log(interpoBoi); 
-                const imageReturn = fs.readFileSync(`./${interpoBoi}`); 
-                response.write(imageReturn); 
-                return response.end(); 
+                console.log(interpoBoi);
+                const imageReturn = fs.readFileSync(`./${interpoBoi}`);
+                response.write(imageReturn);
+                return response.end();
             }
         }
 
         if (request.method === 'POST') {
             if (request.url === '/signBook') {
-                signed.push(request.body); 
-                response.statusCode = 302; 
-                response.setHeader('Location', '/page2.html'); 
-                return response.end(); 
+                signed.push(request.body);
+                response.statusCode = 302;
+                response.setHeader('Location', '/page2.html');
+                return response.end();
             }
         }
 
-        response.statusCode = 404; 
-        response.setHeader('Content-Type', 'text/html'); 
+        response.statusCode = 404;
+        response.setHeader('Content-Type', 'text/html');
         response.end(fs.readFileSync('./index.html', 'utf-8'));
-    }); 
+    });
 });
 
 
